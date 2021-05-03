fcde6965582ab362a39d9b3532d5f7faadc79846
reeved <dcunhareeve@gmail.com>
2021-05-03 15:17:41 +1200

    fix eslint typescript import error (#43)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: ['plugin:react/recommended', 'plugin:import/typescript', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
