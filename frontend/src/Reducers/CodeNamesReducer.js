a2fab215663cb3fe4d03e245eab4d617bead7321
byu621 <55732851+byu621@users.noreply.github.com>
2021-05-09 23:26:39 +1200

    removed comments

--- frontend/src/Reducers/CodeNamesReducer.js
+++ frontend/src/Reducers/CodeNamesReducer.js
@@ -74,8 +74,6 @@ const reducer = (state, action) => {
     }
 
     case 'game-over': {
-      console.log('Game is over');
-      console.log('Winner:', action.winningTeam, 'Team:', state.team, action.winningTeam != state.team);
       return {
         ...state,
         isGameOver: true,
