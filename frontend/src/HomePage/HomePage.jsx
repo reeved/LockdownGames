097f61898c6044da588cfb6c0ddc5e45fd7ff8be
reeved <dcunhareeve@gmail.com>
2021-05-09 18:54:54 +1200

    Home restyle (#58)
    * change socket url
    
    * Change card images to webp format
    
    * change card image folder name
    
    * Home Page Restyle

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,27 +1,39 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-duplicate-props */
 import { React, useState, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Grid, TextField, Button, makeStyles } from '@material-ui/core';
+import { TextField, Button, makeStyles, FormHelperText } from '@material-ui/core';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from './HomePage.module.css';
 import socket from '../Socket';
 import { LobbyContext } from '../Context';
+import { ReactComponent as Illustration1 } from '../home1.svg';
+import { ReactComponent as Illustration2 } from '../home2.svg';
 
 const useStyles = makeStyles({
   textField: {
-    width: '80%',
+    display: 'flex',
+    marginBottom: '1em',
   },
   input: {
-    fontSize: 'calc(10px + 1.4vmin)',
-    backgroundColor: `hsl(34, 57%, 70%)`,
+    fontSize: 'calc(14px + 2vmin)',
+    backgroundColor: `#6d6d6d`,
+  },
+  helperText: {
+    fontSize: '1em',
   },
   button: {
     width: '100%',
     padding: '1em 0 1em 0',
     fontSize: 'calc(10px + 1vmin)',
-    marginTop: '2em',
-    backgroundColor: ({ lobbyCode }) => (lobbyCode ? 'Teal' : 'Coral'),
+    transition: 'all 1s cubic-bezier(0.25, 0.8, 0.25, 1)',
+    marginTop: '2.5em',
+    color: 'white',
+    backgroundColor: ({ lobbyCode }) => (lobbyCode ? '#F72585' : '#5390d9'),
     '&:hover': {
-      backgroundColor: ({ lobbyCode }) => (lobbyCode ? 'CadetBlue' : 'DarkSalmon'),
+      backgroundColor: ({ lobbyCode }) => (lobbyCode ? '#F966A8' : '#75a6e1'),
     },
   },
 });
@@ -35,11 +47,9 @@ const HomePage = () => {
 
   const notValid = lobbyState.lobbyID.length !== 6 && lobbyState.lobbyID !== false;
 
-  let errorMsgText;
+  let errorMsgText = ' ';
 
   const classes = useStyles({ lobbyCode });
-
-  const userHasInput = nickname || lobbyCode;
 
   const handleBtnClick = () => {
     if (lobbyCode) {
@@ -74,51 +84,64 @@ const HomePage = () => {
     return <Redirect to="/lobby" />;
   }
   return (
-    <div className={styles.container}>
-      <h1>LOCKDOWN GAMES</h1>
-      <Grid container spacing={3} justify="center">
-        <Grid container item xs={12} md={4} lg={4} justify="flex-end" alignItems="center" style={{ marginBottom: '1.25rem' }}>
-          <p>Nickname</p>
-        </Grid>
-        <Grid container item xs={12} md={8} lg={8} justify="flex-start">
-          <TextField
-            error={nicknameError}
-            helperText={errorMsg}
-            className={classes.textField}
-            InputProps={{
-              className: `${classes.input}`,
-            }}
-            inputProps={{ maxLength: 10 }}
-            variant="outlined"
-            onChange={(e) => validateNickname(e.target.value)}
-          />
-        </Grid>
-        <Grid container item xs={12} md={4} lg={4} justify="flex-end" alignItems="center">
-          <p>Lobby Code</p>
-        </Grid>
-        <Grid container item xs={12} md={8} lg={8} justify="flex-start">
-          <TextField
-            error={notValid}
-            helperText={errorMsgText}
-            className={classes.textField}
-            InputProps={{
-              className: classes.input,
-            }}
-            inputProps={{ maxLength: 6 }}
-            variant="outlined"
-            value={lobbyCode}
-            onChange={(e) => setCode(e.target.value.toUpperCase())}
-          />
-        </Grid>
-        <Grid item xs={12} md={6} lg={6}>
-          {userHasInput && (
-            <Button type="button" className={`${classes.button}`} variant="contained" disabled={nicknameError || !nickname} onClick={handleBtnClick}>
-              {lobbyCode ? `Join Lobby` : `Create Lobby`}
-            </Button>
-          )}
-        </Grid>
-      </Grid>
-    </div>
+    <Container fluid className={styles.container}>
+      <Row className={styles.row}>
+        <Col xs={3} className={`flexCol ${styles.leftCol}`}>
+          <Illustration1 />
+        </Col>
+        <Col className={`flexCol ${styles.midCol}`}>
+          <h1 className={styles.header}>LOCKDOWN GAMES</h1>
+          <div className={styles.inputContainer}>
+            <div className={styles.inputs}>
+              <h1 className={styles.inputHeader}>LETS GO!</h1>
+              <TextField
+                error={nicknameError}
+                helperText={errorMsg}
+                className={classes.textField}
+                InputProps={{
+                  className: `${classes.input}`,
+                }}
+                FormHelperTextProps={{
+                  className: `${classes.helperText}`,
+                }}
+                inputProps={{ maxLength: 10 }}
+                variant="outlined"
+                placeholder="Enter Nickname"
+                onChange={(e) => validateNickname(e.target.value)}
+              />
+              <TextField
+                error={notValid}
+                helperText={errorMsgText}
+                className={classes.textField}
+                InputProps={{
+                  className: classes.input,
+                }}
+                FormHelperTextProps={{
+                  className: `${classes.helperText}`,
+                }}
+                inputProps={{ maxLength: 6 }}
+                variant="outlined"
+                placeholder="Enter Lobby Code"
+                value={lobbyCode}
+                onChange={(e) => setCode(e.target.value.toUpperCase())}
+              />
+              <Button
+                type="button"
+                className={`${classes.button}`}
+                variant="contained"
+                disabled={nicknameError || !nickname}
+                onClick={handleBtnClick}
+              >
+                {lobbyCode ? `Join Lobby` : `Create Lobby`}
+              </Button>
+            </div>
+          </div>
+        </Col>
+        <Col xs={3} className={`flexCol ${styles.rightCol}`}>
+          <Illustration2 />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
